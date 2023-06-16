a94eca665459703e4ebbc039ec9e8d4440cbd267
VanesaK <minitea@sookmyung.ac.kr>
2023-06-16 19:57:37 +0900

    [ADD] 키워드 검색 api 연동 완료, 검색 결과 페이지 작업 중

--- screens/filter-search/KeywordPages/SeasonPage/index.tsx
+++ screens/filter-search/KeywordPages/SeasonPage/index.tsx
@@ -1,42 +1,165 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-    Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
 } from 'react-native';
 
-import { HeaderArea, BackButton, HeaderTitle, InputArea,KeywordInputSection, SectionTitle,} from './style';
+import { HeaderArea, BackButton, HeaderTitle, InputArea, KeywordInputSection, SectionTitle, KeywordButton, KeywordText, GetRecommendationsButton, GetRecommendationsText, } from './style';
 import { useNavigation } from '@react-navigation/native';
+import ApiService from '../../../../ApiService';
+import { ScrollView } from 'react-native';
+import { Alert } from 'react-native';
 
 const SeasonPage: React.FC = () => {
 
-    const navigation = useNavigation();
-    const goToHome = () => {
-        //@ts-ignore
-        navigation.navigate("Home")
+  const navigation = useNavigation();
+  const goToResults = (keywordList: { id: number; ptag: string; ptag_kr: string; ptagtype_id: number; }[], resultList: { id: number; ptag: string; ptag_kr: string; ptagtype_id: number; }[]) => {
+
+    //@ts-ignore
+    navigation.navigate("FilterSearchResult",{keywordList:keywordList, resultList:resultList})
+
+  }
+
+  type SEASONKEYWORDSTYPE = { id: number; ptag: string; ptag_kr: string, ptagtype_id: number }
+  type FRAGRANCEWHEELKEYWORDSTYPE = { id: number; ptag: string; ptag_kr: string, ptagtype_id: number }
+  type MOODKEYWORDSTYPE = { id: number; ptag: string; ptag_kr: string, ptagtype_id: number }
+
+  const [seasonKeywords, setSeasonKeywords] = useState<SEASONKEYWORDSTYPE[]>([]);
+  const [fragranceWheelKeywords, setFragranceWheelKeywords] = useState<FRAGRANCEWHEELKEYWORDSTYPE[]>([]);
+  const [moodKeywords, setMoodKeywords] = useState<MOODKEYWORDSTYPE[]>([]);
+
+  type KEYWORDTAGSTYPE = { id: number; ptag: string; ptag_kr: string; ptagtype_id: number }
+  const [keywordTagsArray, setKeywordTagsArray] = useState<KEYWORDTAGSTYPE[]>([]);
+  let addOrDeleteKeywordArray: { id: number; ptag: string; ptag_kr: string; ptagtype_id: number; }[] = [];
+
+
+  const getKeywords = () => {
+    ApiService.GETSEARCHSEASONPAGEKEYWORD()
+      .then((data) => {
+
+        let seasonArray = [];
+        let moodArray = [];
+        let fragranceWheelArray = [];
+
+        for (let i = 0; i < data?.data.length; i++) {
+          if (data?.data[i]?.ptagtype_id === 4) {
+            seasonArray.push(data?.data[i])
+          }
+          else if (data?.data[i]?.ptagtype_id === 2) {
+            moodArray.push(data?.data[i])
+          } else if (data?.data[i]?.ptagtype_id === 1) {
+            fragranceWheelArray.push(data?.data[i])
+          } else {
+            return;
+          }
+        }
+        setSeasonKeywords(seasonArray);
+        setMoodKeywords(moodArray);
+        setFragranceWheelKeywords(fragranceWheelArray);
+      }
+      ).catch((res) => {
+        console.log('키워드 받아오기 실패')
+        console.log(res)
+      })
+  }
+
+  useEffect(() => {
+    getKeywords();
+  }, [])
+
+
+  const addOrDeleteKeyword = (el: { id: number; ptag: string; ptag_kr: string; ptagtype_id: number; }) => {
+    let doesExist = addOrDeleteKeywordArray.filter(keyword => keyword?.id === el?.id)
+    if (doesExist?.length === 0) {
+      addOrDeleteKeywordArray.push(el);
+
+    } else {
+      addOrDeleteKeywordArray = addOrDeleteKeywordArray.filter(keyword => keyword?.id !== el?.id)
     }
 
+  }
 
-    return (
-        <View >
-            <HeaderArea>
-                <BackButton onPress={() => navigation.goBack()}>
-                  <Image style={{ position: "absolute" }} source={require('../../../../assets/images/icon/icon-btn-back.png')} />
-                </BackButton>
-                <HeaderTitle>계열/분위기/계절</HeaderTitle >
-              </HeaderArea>
 
-              <InputArea>
-                <KeywordInputSection>
-                    <SectionTitle>계열</SectionTitle>
 
-                </KeywordInputSection>
-              
-              </InputArea>
-        </View>
-    );
+
+
+  const getRecommendations = () => {
+    let keywords=[];
+    for (let i=0;i<addOrDeleteKeywordArray.length;i++){
+      keywords.push('keywordId='+addOrDeleteKeywordArray[i]?.id)
+    }
+    keywords.join('&')
+    let params=keywords.join('&')
+    if (params){
+    ApiService.GETSEARCHKEYWORDRESULT(params)
+      .then((data) => {
+          console.log('결과 값', data.data)
+          goToResults(addOrDeleteKeywordArray, data.data)
+          
+      }).catch((res) => {
+        console.log('결과 받아오기 실패')
+        console.log(res)
+      })
+    }
+    else{
+      Alert.alert('키워드를 선택해 주세요.')
+    }
+  }
+
+  return (
+    <View >
+      <ScrollView>
+        <HeaderArea>
+          <BackButton onPress={() => navigation.goBack()}>
+            <Image style={{ position: "absolute" }} source={require('../../../../assets/images/icon/icon-btn-back.png')} />
+          </BackButton>
+          <HeaderTitle>계절/분위기/계열</HeaderTitle >
+        </HeaderArea>
+
+        <InputArea>
+          <KeywordInputSection>
+            <SectionTitle>계절</SectionTitle>
+            <View style={{ display: "flex", width: "100%", flexDirection: "row", flexWrap: "wrap", alignItems: "flex-start", }}>
+
+              {
+                seasonKeywords.map((el) => <KeywordButton key={el.id} onPress={() => addOrDeleteKeyword(el)}><KeywordText>{el?.ptag_kr}</KeywordText></KeywordButton>)
+              }
+            </View>
+          </KeywordInputSection>
+
+          <KeywordInputSection>
+            <SectionTitle>분위기</SectionTitle>
+            <View style={{ display: "flex", width: "100%", flexDirection: "row", flexWrap: "wrap", alignItems: "flex-start", }}>
+
+              {
+                moodKeywords.map((el) => <KeywordButton key={el.id} onPress={() => addOrDeleteKeyword(el)}><KeywordText>{el?.ptag_kr}</KeywordText></KeywordButton>)
+              }
+            </View>
+          </KeywordInputSection>
+
+
+          <KeywordInputSection >
+            <SectionTitle>계열</SectionTitle>
+            <View style={{ display: "flex", width: "100%", flexDirection: "row", flexWrap: "wrap", alignItems: "flex-start", marginBottom: 70, }}>
+
+              {
+                fragranceWheelKeywords.map((el) => <KeywordButton key={el.id} onPress={() => addOrDeleteKeyword(el)}><KeywordText>{el?.ptag_kr}</KeywordText></KeywordButton>)
+              }
+            </View>
+
+          </KeywordInputSection>
+
+        </InputArea>
+
+      </ScrollView>
+      <GetRecommendationsButton onPress={getRecommendations}>
+        <GetRecommendationsText>추천 받기</GetRecommendationsText>
+      </GetRecommendationsButton>
+    </View>
+  );
 };
 
 export default SeasonPage;
