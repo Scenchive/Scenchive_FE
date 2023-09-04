287635518ea9196d8f0040644fe54987ac8d9988
VanesaK <minitea@sookmyung.ac.kr>
2023-09-04 19:10:12 +0900

    [ADD] 향수 상세페이지 평점, 한글이름, 이미지 추가, 홈화면 이미지 추가, 평점 추가

--- components/home/CarouselSlider/index.tsx
+++ components/home/CarouselSlider/index.tsx
@@ -17,22 +17,24 @@ type PERFUMEDATA = {
   brandName: string;
   perfumeName: string;
   id: Number;
+  perfumeImage:string|null;
+  ratingAvg:Number|null;
 };
 
 
-const CarouselSlider: React.FC<PERFUMEDATA> = ({ perfumeName, brandName, id }) => {
+const CarouselSlider: React.FC<PERFUMEDATA> = ({ perfumeName, brandName, id, perfumeImage, ratingAvg }) => {
 
   const navigation = useNavigation();
   const goToPerfumeDetail = () => {
     console.log('id', id)
     //@ts-ignore
-    navigation.navigate("Stack",{screen:"PerfumeDetail", params:{perfumeId:id, perfumeName:perfumeName, brandName:brandName}})
+    navigation.navigate("Stack",{screen:"PerfumeDetail", params:{perfumeId:id, perfumeName:perfumeName, brandName:brandName, perfumeImage:perfume}})
   }
   return (
-    <View style={{ paddingLeft:20, paddingRight:20}}>
+    <View style={{ paddingLeft:20, paddingRight:20, marginTop:10}}>
       <TouchableOpacity style={{width:"100%", display:"flex", flexDirection:"row"}} onPress={goToPerfumeDetail}>
       {/* <LeftArrowIcon source={require('../../../assets/images/icon/icon-btn-left-arrow.png')} /> */}
-      <PerfumeImage source={require('../../../assets/images/icon/icon-perfume-pic.png')} />
+      <PerfumeImage source={perfumeImage ? { uri: `${perfumeImage}` } : require('../../../assets/images/icon/icon-perfume-pic.png')} />
       {/* <Text style={{ backgroundColor: "#DABDFF" }}>이미지 준비중입니다.</Text> */}
 
       <PerfumeInformationArea>
@@ -41,7 +43,7 @@ const CarouselSlider: React.FC<PERFUMEDATA> = ({ perfumeName, brandName, id }) =
         <BrandEnglish>{brandName}</BrandEnglish>
         <View style={{ display: "flex", flexDirection: "row", marginTop: 4, marginLeft: -3 }}>
           <Image source={require('../../../assets/images/icon/icon-yellow-star.png')} />
-          <Text style={{ fontSize: 14, color: "#2E2E2E" }}>4.8/5</Text>
+          <Text style={{ fontSize: 14, color: "#2E2E2E" }}>{ratingAvg? `${ratingAvg}`:"리뷰없음"}/5</Text>
         </View>
 
       </PerfumeInformationArea>
